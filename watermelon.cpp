214d5890dac6cb060b7e523913d7f5a3d3178759
Aretosteles <76232104+Aretosteles@users.noreply.github.com>
2020-12-20 12:51:34 +0100

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,22 @@
+#import<iostream>
+using namespace std;
+
+main(int n){
+
+  cin>>n; //input
+  puts(n<3|n%2?"NO":"YES");
+
+  /*
+  OUTPUT
+  Print YES, if the boys can divide the watermelon into two parts,
+  each of them weighing even number of kilos; and NO in the opposite case.
+
+  EXPLANATION
+  conditionl operator
+  condition ? result_if_true : result_if_false
+  Prints NO if watermelon less than 3 OR there's a remainder in modulo division
+
+
+  */
+
+}
